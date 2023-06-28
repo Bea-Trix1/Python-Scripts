676b6dd8cad4448bc71b3c471f721bd1ce40c273
Bea-Trix1 <beatriz.ferrante37@gmail.com>
2023-06-28 14:12:52 -0300

    Primeiro Commit de scripts python feito em sala de aula.

---
+++ Python scripts/exercicio1.py
@@ -0,0 +1,7 @@
+nome = input('Qual seu nome?\n')
+idade = input('Qual sua idade?\n')
+peso = input('Qual seu peso?\n')
+
+print ('Seu nome é:',nome,'\n''Sua idade é:',idade,'\n' 'Seu peso é:',peso) 
+
+input()
